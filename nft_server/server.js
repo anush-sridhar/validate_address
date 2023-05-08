fee10075d3183e853b2d0a68314d13a08b701e74
Anush Sridhar <anush@eduroamverw-130-82-24-144.unisg.ch>
2023-05-08 16:54:54 +0200

    server and vercel change

--- nft_server/server.js
+++ nft_server/server.js
@@ -105,7 +105,8 @@ app.post('/getRes', async (req, res) =>{
     const data = JSON.stringify(result.data);
     console.log(`got res: ${data}`);
     console.log(`image url: ${data['image']}`);
-    console.log(`rarity: ${data['attributes'][0]['value']}`);
+    console.log(`rarity: ${data['attributes']);
+    //console.log(`rarity: ${data['attributes'][0]['value']}`);
     res.status(200).json(data);
   } catch (error) {
     console.log("Error:", error.message);
